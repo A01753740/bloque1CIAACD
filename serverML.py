f6b9ed9bf79313b37bfb2b2117a502ede0e79299
DavidClemente21 <a01753740@tec.mx>
2024-08-27 18:54:11 -0600

    Servidor en python-flask

--- serverML.py
+++ serverML.py
@@ -13,3 +13,21 @@ server = Flask(__name__)
 
 # Define a route to send JSON data
 @server.route('/predictjson', methods=['POST']) # Send data to the server
+
+def predictjson():
+    # Process the input data
+    data = request.json
+    print(data)
+    inputData = np.array([
+        data['pH'],
+        data['sulphates'],
+        data['alcohol']
+    ])
+
+    # Make a prediction
+    result = df.predict([inputData.reshape(1, -1)])
+    # Return the prediction
+    return jsonify({'Prediction': str(result[0])})
+
+if __name__ == '__main__':
+    server.run(debuj='False', host='0.0.0.0', port=8080)
